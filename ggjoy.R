1c3faa94158f0bfb063e2ab3cfda7d5a52b0616c
lbenz730 <luke.benz@yale.edu>
2017-12-15 15:34:55 -0500

    Joy Plot Code

---
+++ ggjoy.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(ggjoy)
+library(dplyr)
+library(viridis)
+
+x <- read.csv("2.0_Files/Power_Rankings/Powerrankings.csv", as.is = T)
+x %>% mutate(group = reorder(Conference, YUSAG_Coefficient, median)) %>%
+  ggplot(aes(x = YUSAG_Coefficient, y = group, fill = ..x..)) + 
+  geom_joy_gradient(scale = 3) + theme_joy() +
+  scale_y_discrete(expand = c(0.01, 0)) + 
+  scale_x_continuous(expand = c(0.01, 0)) +
+  theme(legend.position="none") +
+  scale_fill_viridis(name = "group", option = "C") +
+  labs(y = "Conference", title = "NCAA Men's Basketball Power Rankings")
