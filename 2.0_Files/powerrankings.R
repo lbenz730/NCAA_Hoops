4db142619192df35f27bcd6b2e9d232010c5e2ca
lbenz730 <luke.benz@yale.edu>
2018-01-09 12:38:10 -0500

    Scale w/out Changing Coefficients

--- 2.0_Files/powerrankings.R
+++ 2.0_Files/powerrankings.R
@@ -1,15 +1,19 @@
 pr_compute <- function(by_conf) {
+  avg <- mean(lm.hoops$coefficients[2:351])
+  opp_avg <- mean(lm.hoops$coefficients[352:701])
+  
   powerrankings <- data.frame(Team = rep(NA, length(teams)),
                               Conference = rep(NA, length(teams)),
                               YUSAG_Coefficient = rep(NA, length(teams)))
-  powerrankings[1, ] <- c(teams[1], "Southland", lm.hoops$coefficients[1] + lm.hoops$coefficients[702])
+  powerrankings[1, ] <- c(teams[1], "Southland", lm.hoops$coefficients[1] - mean(avg, abs(opp_avg)))
+  
   
   ### Get YUSAG Coefficients
   for(i in 2:(length(teams))) {
     teamcoef <- 
-      lm.hoops$coefficients[paste("team", teams[i], sep = "")] 
+      lm.hoops$coefficients[paste("team", teams[i], sep = "")] - mean(avg, abs(opp_avg))
     opponentcoef <- 
-      lm.hoops$coefficients[paste("opponent", teams[i], sep = "")]
+      lm.hoops$coefficients[paste("opponent", teams[i], sep = "")] + mean(avg, abs(opp_avg))
     tmp <- c(teams[i], get_conf(teams[i]), round((teamcoef - opponentcoef)/2, 2))
     powerrankings[i, ] <- tmp
   }
