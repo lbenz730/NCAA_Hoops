fb0d3328b974717615ea42e9d60fa406aee22ce6
lbenz730 <luke.benz@yale.edu>
2018-01-14 14:01:58 -0500

    System Update 1/14/2018

--- Model 2.0.R
+++ Model 2.0.R
@@ -1,7 +1,7 @@
 #############################  Read CSVs #######################################
 library(dplyr)
 x <- read.csv("2.0_Files/Results/2016-17/NCAA_Hoops_Results_6_29_2017.csv", as.is = T)
-y <- read.csv("2.0_Files/Results/2017-18/NCAA_Hoops_Results_1_11_2018.csv", as.is = T)
+y <- read.csv("2.0_Files/Results/2017-18/NCAA_Hoops_Results_1_14_2018.csv", as.is = T)
 mins <- read.csv("2.0_Files/Info/mins.csv", as.is = T)
 rec <- read.csv("2.0_Files/Info/recruiting.csv", as.is = T)
 transfers <- read.csv("2.0_Files/Info/transfers.csv", as.is = T)
@@ -147,7 +147,7 @@ sum(sign(history$predscorediff) == sign(history$scorediff), na.rm = T)/sum(!is.n
 playoffs <- ivy.sim(nsims = 5000)
 simresults <- fast.sim(nsims = 20000)
 ivy_joy(simresults)
-psf_results <- psf(nsims = 2500, year = 2018, months = c(1,1), days = c(5,6))
+psf_results <- psf(nsims = 2500, year = 2018, months = c(1,1), days = c(12,13))
 
 ############################# Conference Sims (No Tie-Breaks) ##################
 conf_results <- conf_sim("SEC", 10000)
