52e0f96902cba5e1610489e88934c4dc14edeb62
lbenz730 <luke.benz@yale.edu>
2018-01-16 12:09:26 -0500

    System Update 1/16/18

--- Model 2.0.R
+++ Model 2.0.R
@@ -1,7 +1,7 @@
 #############################  Read CSVs #######################################
 library(dplyr)
 x <- read.csv("2.0_Files/Results/2016-17/NCAA_Hoops_Results_6_29_2017.csv", as.is = T)
-y <- read.csv("2.0_Files/Results/2017-18/NCAA_Hoops_Results_1_14_2018.csv", as.is = T)
+y <- read.csv("2.0_Files/Results/2017-18/NCAA_Hoops_Results_1_16_2018.csv", as.is = T)
 mins <- read.csv("2.0_Files/Info/mins.csv", as.is = T)
 rec <- read.csv("2.0_Files/Info/recruiting.csv", as.is = T)
 transfers <- read.csv("2.0_Files/Info/transfers.csv", as.is = T)
@@ -148,6 +148,16 @@ playoffs <- ivy.sim(nsims = 5000)
 simresults <- fast.sim(nsims = 20000)
 ivy_joy(simresults)
 psf_results <- psf(nsims = 2500, year = 2018, months = c(1,1), days = c(12,13))
+
+######################### Conf Undefeated Watch ################################
+confs <- confs[order(confs$team), ]
+confs$conf_loss <- aggregate(1-wins ~ team, data = filter(y, conf_game, !is.na(scorediff)), sum)[,2]
+undefeateds <- filter(confs, conf_loss == 0)
+undefeateds$prob <- NA
+for(i in 1:nrow(undefeateds)) {
+  undefeateds$prob[i] <- prod(y$wins[is.na(y$scorediff) & y$conf_game & y$team == undefeateds$team[i]])
+}
+write.csv(undefeateds[,c(1,2,7)], "2.0_Files/Predictions/undeafeateds.csv", row.names = F)
 
 ############################# Conference Sims (No Tie-Breaks) ##################
 conf_results <- conf_sim("SEC", 10000)
