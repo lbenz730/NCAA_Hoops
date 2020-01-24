6e0abcc70b9e48f1ba117c27e10364ccaa189326
lbenz730 <luke.benz@yale.edu>
2020-01-24 10:33:08 -0500

    System Update

--- update_site.sh
+++ update_site.sh
@@ -1,2 +1,8 @@
-Rscript ncaa_hoops_scraper.R
-Rscript Model_3.0.R
+#!/usr/bin/env Rscript
+
+now = $(date)
+#Rscript ncaa_hoops_scraper.R
+#Rscript Model_3.0.R
+git add *
+git commit -m "System Update $now" 
+git push
