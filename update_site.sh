0e814db0b4fec87a1cad6e003f4e68ea1e6aa571
lbenz730 <luke.benz@yale.edu>
2020-01-24 10:41:40 -0500

    System Update

--- update_site.sh
+++ update_site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-now = `date` +"%y-%m-%d"
+date_path = `date` +"%y-%m-%d"
 #Rscript ncaa_hoops_scraper.R
 #Rscript Model_3.0.R
 git add *
-git commit -m "System Update ${now}" 
+git commit -m "System Update ${date_path}" 
 git push
