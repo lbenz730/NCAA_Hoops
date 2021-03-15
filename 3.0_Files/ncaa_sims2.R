4354e6c0666569b89a08680cb82c1e87b5eb41d3
lbenz730 <luke.benz@yale.edu>
2021-03-14 23:16:07 -0400

    System Update 2021-03-14

--- 3.0_Files/ncaa_sims2.R
+++ 3.0_Files/ncaa_sims2.R
@@ -4,13 +4,33 @@ library(furrr)
 options(future.fork.enable = T)
 
 ### Number of sims to run
-n_sims <- 100
+n_sims <- 10000
 set.seed(13579)
 
 ### Function to sim games 
 sim_game <- function(team, opponent, win_prob) {
- ifelse(runif(1) <= win_prob, team, opponent)
+  ifelse(runif(1) <= win_prob, team, opponent)
 }
+
+### Function to Build a Bracket For a Round
+build_bracket <- function(seed_list) {
+  n <- nrow(seed_list)
+  ix <- seq(1, n, 2)
+  tibble('team' = seed_list$team[ix],
+         'opponent' = seed_list$team[-ix]) %>% 
+    inner_join(wp_matrix, by = c('team', 'opponent'))
+  
+}
+
+### Function to Simulate Games in Round
+sim_round <- function(bracket) {
+  pmap_chr(list('team' = bracket$team,
+                'opponent' = bracket$opponent,
+                'win_prob' = bracket$win_prob),
+           sim_game)
+}
+
+
 
 ### Read in Seed list and team ratings
 seed_list <- read_csv('seed_list.csv')
@@ -41,17 +61,72 @@ wp_matrix <-
 first_four <- 
   seed_list %>% 
   filter(first_four) %>% 
-  group_by(region, seed) %>%
-  group_map(~tibble('team' = .x$team[1], 
-                    'opponent' = .x$team[2],
-                    'seed' = .x$seed[1],
-                    'region' = .x$region[1]),
-            .keep = T) %>% 
-  bind_rows() %>% 
-  inner_join(wp_matrix, by = c('team', 'opponent'))
+  build_bracket()
+first_four <- map(1:n_sims, ~{first_four})
+
+first_four_winners <- future_map(first_four, sim_round)
+
+### Brackets for Tournament Proper
+ncaa_brackets <- 
+  map(first_four_winners, ~{
+    filter(seed_list, !first_four | team %in% .x) 
+  })
+
+### Keep Track of Winners
+round_winners <- list()
+
+### Simulate Tournament
+for(i in 1:6) {
+  cat('Simulating Round', i, 'of', 6, '\n')
+  ### in bracket Form
+  brackets <- future_map(ncaa_brackets, build_bracket)
+  winners <- future_map(brackets, sim_round)
+  round_winners[[i]] <- unlist(winners)
+  ncaa_brackets <- map2(ncaa_brackets, winners, ~{filter(.x, team %in% .y)})
+}
+
+### Aggregate Results
+sim_results <- 
+  seed_list %>% 
+  select(-elim_round) %>% 
+  group_by(team) %>% 
+  mutate('first_round' = ifelse(!first_four, 1, sum(team == unlist(first_four_winners))/n_sims),
+         'second_round' = sum(team == round_winners[[1]])/n_sims,
+         'sweet_sixteen' = sum(team == round_winners[[2]])/n_sims,
+         'elite_eight' = sum(team == round_winners[[3]])/n_sims,
+         'final_four' = sum(team == round_winners[[4]])/n_sims,
+         'championship_game' = sum(team == round_winners[[5]])/n_sims,
+         'champ' = sum(team == round_winners[[6]])/n_sims) %>% 
+  ungroup() %>% 
+  select(-first_four) %>% 
+  arrange(-champ, -championship_game, 
+          -final_four, -elite_eight, -sweet_sixteen,
+          -second_round, -first_round) 
+  
+  
 
 
-map_dfr(
-            
-            
-            
+library(gt)
+sim_results %>% 
+  gt() %>% 
+  data_color(
+    columns = vars(rating),
+    colors = scales::col_numeric(
+      palette = c("#ffffff", "#f2fbd2", "#c9ecb4", "#93d3ab", "#35b0ab"),
+      domain = NULL,
+    )
+  ) %>% 
+  fmt_number(
+    columns = vars(rating),
+    decimals = 1
+    ) %>% 
+  fmt_percent(
+    columns = vars(first_round, second_round,
+                     sweet_sixteen, elite_eight,
+                     final_four, championship_game,
+                     champ),
+    # digits = 1,
+    decimals = 0,
+    pattern = "{x}")
+  
+
